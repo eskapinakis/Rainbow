53dd35686ee9a2e79e4435c5afb9213ece85c35b
taketshi <83816861+taketshi@users.noreply.github.com>
2021-05-10 14:13:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 from rainbow import Rainbow
 
-order = 0
-# R = Rainbow.Rainbow()
-
-
-def interact():
-    option = input("choose an option: ")
-    if option == "exit":
-        return
-    else:
-        x1 = int(input("first element: "))
-        x2 = int(input("second element: "))
-        print(R.op(x1, x2, option))
-        interact()
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    R = Rainbow.Rainbow()
+    R = Rainbow.Rainbow(8)
 
-    x = [0]*33
-    #print(R.F(x))
-    #print("")
-    #print(R.eta)
+    x = [2*i for i in range(R.n-R.v[0])]
 
-    # interact()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print( R.FTilde( R.findSolution(x) ) )
+    print(R.verify(x,R.sign(x)))
